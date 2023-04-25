d13b2b9d0ce7e2380196640a85dd9252e540c1f0
Brett Abramson <brett.m.abramson@gmail.com>
2023-04-25 11:31:30 -0500

    R funcitoning

--- components/add_album.js
+++ components/add_album.js
@@ -1,29 +1,29 @@
 import axios from "axios";
-import { useState } from "react"
-import useSWR, { mutate } from "swr"
+import { useState } from "react";
+import useSWR, { mutate } from "swr";
 
-const AddAlbum = (props) => {
-  const endpoint = "http://localhost:8000/api/albums"
-  const { data, error } = useSWR(endpoint)
+const AddAlbum = () => {
+  const endpoint = "http://localhost:8000/api/albums";
+  const { data, error } = useSWR(endpoint);
   const [formData, setFormData] = useState({
     artist: "",
     title: "",
     year_released: "",
-  })
+  });
 
   const handleChange = (event) => {
     setFormData({
       ...formData,
-      [event.target.name] : event.target.value,
-    })
-  }
+      [event.target.name]: event.target.value,
+    });
+  };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
     let newAlbum = {
       artist: formData.artist,
       title: formData.title,
-      year_released: formData.year_released
+      year_released: formData.year_released,
     };
     try {
       const response = await axios.post(endpoint, newAlbum, {
@@ -32,40 +32,59 @@ const AddAlbum = (props) => {
         },
       });
 
-      console.log(response)
-      mutate(endpoint)
+      console.log(response);
+      mutate(endpoint);
 
       setFormData({
-        artist:"",
-        title:"",
-        year_released:"",
-      })
-
-    }
-    catch (error) {
-      console.error(error)
-      alert("Error Adding Album")
+        artist: "",
+        title: "",
+        year_released: "",
+      });
+    } catch (error) {
+      console.error(error);
+      alert("Error Adding Album");
     }
   };
 
   return (
-    
     <form onSubmit={handleSubmit}>
       <label htmlFor="artist">Artist</label>
-      <input type="text" id="artist" name="artist" value={formData.artist} onChange={handleChange} required />
+      <input
+        type="text"
+        id="artist"
+        name="artist"
+        value={formData.artist}
+        onChange={handleChange}
+        required
+      />
 
       <label htmlFor="title">Album Title</label>
-      <input type="text" id="title" name="title" value={formData.title} onChange={handleChange} required />
+      <input
+        type="text"
+        id="title"
+        name="title"
+        value={formData.title}
+        onChange={handleChange}
+        required
+      />
 
       <label htmlFor="year_released">Year of Albums Release</label>
-      <input type="number" id="year_released" name="year_released" value={formData.year_released} onChange={handleChange} required />
+      <input
+        type="number"
+        id="year_released"
+        name="year_released"
+        value={formData.year_released}
+        onChange={handleChange}
+        required
+      />
 
-      <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      <button
+        type="submit"
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
         Submit
       </button>
-
     </form>
-
   );
 };
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,87 +2,49 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import Link from "next/link";
 import AddAlbum from "../../components/add_album";
-import useSWR from "swr";
-import ListAlbums from "../../components/list_albums";
-
-const Home = ({ albums }) => {
-  // this is the GET
-  const [albumList, setAlbumList] = useState([])
-  const fetcher = (url) => axios.get(url).then((res) => res.data);
-  const endpoint = "http://localhost:8000/api/albums"
-  const { data, error, mutate } = useSWR(endpoint, fetcher);
-  
 
 
-  const handleDelete = (event) => {
+const Home = ({  }) => {
+  let [albumList, setAlbumList] = useState([])
+
+  const getAlbums = () => {
     axios
-      .delete(endpoint + "/" + event.target.value)
-      .then(() => {
-        endpoint,
-        (data) => {
-          return data.filter((album) => album.id !== id)
-        }, 
-        false
-      })
-  }
-  const displayAlbums = () => {
-    if (error) return <div> {error.message} </div>
-    if (!data) return <div> Loading... </div>
-    
-    return (
-      data.map((album, index) => {
-        return (
-          <div key={index} className="prose">
-            <h3>{album.artist}</h3>
-            <h4>{album.title}</h4>
-            <h5>{album.year_released}</h5>
-            <button onClick={handleDelete} value={album.id}>Remove Album</button>
-          </div>
-        );
-      })
-    )
+      .get("http://localhost:8000/api/albums")
+      .then(
+        (response) => setAlbumList(response.data),
+        (err) => console.error(err)
+      )
+      .catch((error) => console.error(error))
   }
 
+
+  useEffect(() => {
+    getAlbums()
+  }, [])
 
   return (
     <>
+
       <h1 className="text-2xl">Album Assist</h1>
-      {console.log(data)}
-      {/* <div>
-        {error ? (
-          <div>return ({error.message}) </div>
-        ) : data ? (
-          data.map((album, index) => {
-            return (
-              <div key={index}>
-                <h2>{album.artist}</h2>
-                <h3>{album.title}</h3>
-                <h5>{album.year_released}</h5>
-              </div>
-            );
-          })
-        ) : (
-          <div> Loading... </div>
-        )}
-      </div> */}
-      {displayAlbums()}
       <br />
+      <div>
+        {albumList.map((album, index) => {
+          return (
+            <div key={index}>
+              <h3>{album.artist}</h3>
+              <h3>{album.title}</h3>
+              <h4>{album.year_released}</h4>
+              <br />
+            </div>
+            
+          )
+        })}
+      </div>
       <h2 className="text-lg">Add Album</h2>
       <AddAlbum />
+
     </>
   );
 };
-
-// export async function getServerSideProps() {
-
-//   const res = await axios.get("http://127.0.0.1:8000/api/albums")
-//   const albums =  res.data
-
-//   return {
-//     props: {
-//       albums
-//     }
-//   }
-// }
 
 export default Home;
