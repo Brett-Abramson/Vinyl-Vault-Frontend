bee9ace572b5797fbf8c10b143a46ed85b504792
Brett Abramson <brett.m.abramson@gmail.com>
2023-04-28 11:41:45 -0500

    APISearch now displaying content on Spotify page

--- src/pages/spotify.js
+++ src/pages/spotify.js
@@ -1,58 +1,83 @@
 const axios = require("axios");
-
-const client_id = process.env.NEXT_PUBLIC_CLIENT_ID;
-const client_secret = process.env.NEXT_PUBLIC_CLIENT_SECRET;
-let token = "";
-
-// requesting authorization
-let authOptions = {
-  method: "post",
-  url: "https://accounts.spotify.com/api/token",
-  headers: {
-    Authorization:
-      "Basic " +
-      new Buffer.from(client_id + ":" + client_secret).toString("base64"),
-    "Content-Type": "application/x-www-form-urlencoded",
-  },
-  data: "grant_type=client_credentials",
-};
-
-const getToken = () => {
-  axios(authOptions)
-    .then((response) => {
-      console.log(response.data);
-      token = response.data.access_token;
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-};
-
-const searchAlbums = async (query) => {
-  // const token = await getToken();
-  console.log(token);
-  const response = await axios.get("https://api.spotify.com/v1/search", {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-    params: {
-      q: "get on board",
-      type: "album",
-      market: "US",
-    },
-  });
-  console.log(response.data);
-  return response.data;
-};
+import { useState } from "react";
 
 const Spotify = () => {
+  const client_id = process.env.NEXT_PUBLIC_CLIENT_ID;
+  const client_secret = process.env.NEXT_PUBLIC_CLIENT_SECRET;
+  let token = "";
+
+  const [albums, setAlbums] = useState([]);
+
+  // requesting authorization
+  let authOptions = {
+    method: "post",
+    url: "https://accounts.spotify.com/api/token",
+    headers: {
+      Authorization:
+        "Basic " +
+        new Buffer.from(client_id + ":" + client_secret).toString("base64"),
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    data: "grant_type=client_credentials",
+  };
+
+  const getToken = () => {
+    axios(authOptions)
+      .then((response) => {
+        console.log(response.data);
+        token = response.data.access_token;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const searchAlbums = async (query) => {
+    // const token = await getToken();
+    console.log(token);
+    const response = await axios.get("https://api.spotify.com/v1/search", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      params: {
+        q: "get on board",
+        type: "album",
+        market: "US",
+        limit: "10",
+      },
+    });
+    // console.log(response.data);
+    setAlbums(response.data.albums.items);
+    return response.data;
+  };
+
+  
+
   return (
     <>
       <button onClick={getToken}>Token</button>
       <br />
       <button onClick={searchAlbums}>Search albums</button>
       <br />
-      <button onClick={console.log(token)}>log token</button>
+      <button onClick={()=>{console.log(albums)}}>whats in albums</button>
+      <div>
+        {albums.map((album) => {
+            return (
+                <div key={album.id}>
+                    <h2>{album.name}</h2>
+                    <p>Artists</p>
+                    {album.artists.map((artist) => {
+                        return(
+                            <div key={artist.id}>
+                                <h3>{artist.name}</h3>
+                            </div>
+                        )
+                    })}
+                    <br />
+                </div>
+            )
+        })}
+      </div>
     </>
   );
 };
