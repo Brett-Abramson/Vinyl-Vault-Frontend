b8aeac10f669da36fcfa2fcb9f85fa16caadfa8b
Brett Abramson <brett.m.abramson@gmail.com>
2023-06-21 18:11:28 -0700

    cleaning up, moved funciton passing to props in Spotify

--- src/components/AlbumItem.js
+++ src/components/AlbumItem.js
@@ -1,11 +1,11 @@
-const AlbumItem = ({ album, handleAddAlbum }) => {
+const AlbumItem = (props) => {
 
   return (
     <div className="flex justify-between flex-col bg-analogous_two hover:bg-primary shadow-md hover:scale-105 hover:shadow-xl transform transition duration-300 ease-in-out my-5 p-5 aspect-square rounded-lg">
       <br />
-      <h2 className="text-2xl font-oswald font-bold">{album.name}</h2>
+      <h2 className="text-2xl font-oswald font-bold">{props.album.name}</h2>
       <div>
-        {album.artists.map((artist) => {
+        {props.album.artists.map((artist) => {
           return (
             <div key={artist.id}>
               <p className="font-lg font-oswald">{artist.name}</p>
@@ -14,7 +14,7 @@ const AlbumItem = ({ album, handleAddAlbum }) => {
         })}
       </div>
       <button
-        onClick={() => handleAddAlbum(album)}
+        onClick={() => props.handleAddAlbum(props.album)}
         className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded"
       >
         Add to Owned Albums
